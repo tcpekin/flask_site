c39afc3b37e723c0a46446721ec20730ea8d94bd
Tom Pekin <tcpekin@gmail.com>
2022-10-25 14:31:34 +0200

    small changes

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker compose -f docker-compose.prod.yaml)" ]; then
-  echo 'Error: docker compose -f docker-compose.prod.yaml is not installed.' >&2
+if ! [ -x "$(command -v docker compose -f docker-compose.dev.yaml)" ]; then
+  echo 'Error: docker compose -f docker-compose.dev.yaml is not installed.' >&2
   exit 1
 fi
 
-domains=(tcpekin.com)
+domains=(tcpekin.com www.tcpekin.com)
 rsa_key_size=4096
 data_path="./services/certbot"
 email="tcpekin@gmail.com" # Adding a valid address is strongly recommended
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.dev.yaml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +39,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker compose -f docker-compose.prod.yaml up --force-recreate -d nginx
+docker compose -f docker-compose.dev.yaml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.dev.yaml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,15 +66,16 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.dev.yaml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
+    --no-eff-email \
     --force-renewal" certbot
 echo
 
 echo "### Reloading nginx ..."
-docker compose -f docker-compose.prod.yaml exec nginx nginx -s reload
+docker compose -f docker-compose.dev.yaml exec nginx nginx -s reload
