c0b08b3204de9016f1137c28e795060fd108a804
Tom Pekin <tcpekin@gmail.com>
2022-10-25 12:04:36 +0200

    take 2

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose -f docker-compose.dev.yaml)" ]; then
-  echo 'Error: docker-compose -f docker-compose.dev.yaml is not installed.' >&2
+if ! [ -x "$(command -v docker compose -f docker-compose.prod.yaml)" ]; then
+  echo 'Error: docker compose -f docker-compose.prod.yaml is not installed.' >&2
   exit 1
 fi
 
-domains=(tcpekin.com www.tcpekin.com)
+domains=(tcpekin.com)
 rsa_key_size=4096
 data_path="./services/certbot"
 email="tcpekin@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -19,7 +19,7 @@ if [ -d "$data_path" ]; then
 fi
 
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then 
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose -f docker-compose.dev.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +39,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose -f docker-compose.dev.yaml up --force-recreate -d nginx
+docker compose -f docker-compose.prod.yaml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose -f docker-compose.dev.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,7 +66,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose -f docker-compose.dev.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -77,4 +77,4 @@ docker-compose -f docker-compose.dev.yaml run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose -f docker-compose.dev.yaml exec nginx nginx -s reload
+docker compose -f docker-compose.prod.yaml exec nginx nginx -s reload
