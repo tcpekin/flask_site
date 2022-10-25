d56c400c00a815fec830ab0e11a757c3b502c546
Ubuntu <tcpekin@gmail.com>
2022-10-25 13:58:59 +0000

    https is working, needed just to chmod everything to ubuntu:ubuntu

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker compose -f docker-compose.dev.yaml)" ]; then
-  echo 'Error: docker compose -f docker-compose.dev.yaml is not installed.' >&2
+if ! [ -x "$(command -v docker compose -f docker-compose.prod.yaml)" ]; then
+  echo 'Error: docker compose -f docker-compose.prod.yaml is not installed.' >&2
   exit 1
 fi
 
@@ -9,7 +9,7 @@ domains=(tcpekin.com www.tcpekin.com)
 rsa_key_size=4096
 data_path="./services/certbot"
 email="tcpekin@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker compose -f docker-compose.dev.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +39,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker compose -f docker-compose.dev.yaml up --force-recreate -d nginx
+docker compose -f docker-compose.prod.yaml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker compose -f docker-compose.dev.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,7 +66,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker compose -f docker-compose.dev.yaml run --rm --entrypoint "\
+docker compose -f docker-compose.prod.yaml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -78,4 +78,4 @@ docker compose -f docker-compose.dev.yaml run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker compose -f docker-compose.dev.yaml exec nginx nginx -s reload
+docker compose -f docker-compose.prod.yaml exec nginx nginx -s reload
